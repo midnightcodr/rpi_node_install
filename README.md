39879bf35aec7091958d73c7ce57284f9074e487
Rico Chen <ricochen27@gmail.com>
2012-11-11 15:53:44 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+node installation one-liner for Debian Squeeze on Raspberry Pi.
+
+# Examine the script
+curl -L https://raw.github.com/midnightcodr/rpi_node_install/master/setup.sh
+
+# Install
+curl -L https://raw.github.com/midnightcodr/rpi_node_install/master/setup.sh | bash -s
---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+orig_dir=$(pwd)
+work_dir=/tmp1
+patch_dir=$work_dir/node-v0.8.14/deps/v8/
+patch_file=$work_dir/node-v0.8.14/deps/v8/SConstruct.patch
+cd $work_dir
+wget -O - http://nodejs.org/dist/v0.8.14/node-v0.8.14.tar.gz|tar xz
+echo "--- SConstruct	2012-10-25 16:49:32.000000000 -0400
++++ SConstruct.modified	2012-11-11 12:33:00.136775230 -0500
+@@ -80,7 +80,7 @@
+   },
+   'gcc': {
+     'all': {
+-      'CCFLAGS':      ['\$DIALECTFLAGS', '\$WARNINGFLAGS'],
++      'CCFLAGS':      ['\$DIALECTFLAGS', '\$WARNINGFLAGS', '-march=armv6'],
+       'CXXFLAGS':     ['-fno-rtti', '-fno-exceptions'],
+     },
+     'visibility:hidden': {
+@@ -159,12 +159,12 @@
+       },
+       'armeabi:softfp' : {
+         'CPPDEFINES' : ['USE_EABI_HARDFLOAT=0'],
+-        'vfp3:on': {
+-          'CPPDEFINES' : ['CAN_USE_VFP_INSTRUCTIONS']
+-        },
+-        'simulator:none': {
+-          'CCFLAGS':     ['-mfloat-abi=softfp'],
+-        }
++#        'vfp3:on': {
++#          'CPPDEFINES' : ['CAN_USE_VFP_INSTRUCTIONS']
++#        },
++#        'simulator:none': {
++#          'CCFLAGS':     ['-mfloat-abi=softfp'],
++#        }
+       },
+       'armeabi:hard' : {
+         'CPPDEFINES' : ['USE_EABI_HARDFLOAT=1'],
+" > $patch_file
+cd $patch_dir
+patch -p0 < SConstruct.patch
+cd ../..
+./configure && make && make install
+cd $orig_dir
